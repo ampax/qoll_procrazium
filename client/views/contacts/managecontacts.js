c0eaa3505fb255d85839653c52ef719971b15cfc
kaushik1979 <kaushik.anoop@gmail.com>
2014-01-16 22:37:25 -0500

    Adding manage contacts/create group template

---
+++ client/views/contacts/managecontacts.js
@@ -0,0 +1,48 @@
+var filename = "client/views/contacts/managecontacts.js"
+
+
+Template.managecontacts.events({
+    'click .create-group': function(event){
+        var groupName = $("input.create-group").val();
+
+        var emails = new Array();
+        $('.email-group-panel').each(function(){
+            qlog.info('found email: ' + $(this).html());
+            emails.push($(this).html());
+            //$(this).remove();
+        });
+
+
+        qlog.info('Sending the following emails for group updated: ' + emails + ', @group: ' + groupName, filename);
+
+        if(groupName && emails.length > 0){
+            //SEND to server for updating user profiles
+            qlog.info('Sending to server ' + groupName + ', ' + emails, filename);
+            
+            Meteor.call("updateUserGroup", groupName, emails, function(error){
+                if(!error){ 
+                    qlog.info("Updated users with group", filename);
+                } else {
+                    qlog.info("Failed to create group: " + error, filename);
+                }
+                
+            });
+        }
+
+        $('.email-group-panel').remove();
+        $("input.create-group").val('');
+        $('.add-email').val('');
+    },
+});
+
+/** jQuery functionality will come here **/
+Template.managecontacts.rendered = function() {
+    qlog.info('clicked on add-email', filename);
+    jQuery('.add-email').click(function(){
+        var email = $("#addemail").val();
+        qlog.info("Adding new email to the group: "+email, filename);
+
+        jQuery('#addedemails').append("<div class='email-group-panel' id='emailtogrouppanel'>"+email+"</div>");
+        $("#addemail").val('');
+    });
+}
--- server/db/UserDb.js
+++ server/db/UserDb.js
@@ -1,20 +1,69 @@
 var filename = 'server/db/UserDb.js';
 
-Users = new Meteor.Collection("USERS");
-
 Meteor.methods({
 	currentUserName: function(){
         qlog.info("Getting avatar for: " + Meteor.userId(), filename);
-        return Users.find({_id : Meteor.userId()}).profile.name;
+        return Meteor.users.find({_id : Meteor.userId()}).profile.name;
     },
 
     currentUserEmail: function(){
         qlog.info("Getting user email for: " + Meteor.userId(), filename);
-        return Users.find({_id : Meteor.userId()}).profile.email;
+        return Meteor.users.find({_id : Meteor.userId()}).profile.email;
     },
 
     currentUserAvatar: function(){
         qlog.info("Getting avatar for: " + Meteor.userId(), filename);
-        return Users.find({_id : Meteor.userId()}).profile.avatar_url;
+        return Meteor.users.find({_id : Meteor.userId()}).profile.avatar_url;
     },
+    updateUserGroup: function(groupName, userEmails) {
+        userEmails.map(function(userEmail){
+            //qlog.info('Updating the user for groups: *' + userEmail + '*, groupName: ' + groupName, filename);
+            //Mongo Query: - db.users.find({'emails.address': {$regex:'abc',$options:'i'}});
+            //var user = Users.findOne({'emails.address': {$regex:userEmail,$options:'i'}});
+            updateUserGroupWithEmail(groupName, userEmail);
+        });
+
+        updateUserGroupsCreated(groupName);
+    }
 });
+
+
+updateUserGroupWithEmail = function(groupName, userEmail){
+    var user=Meteor.users.findOne({ "emails.address" : userEmail });
+    if(!user.groups) {
+        user.groups = {};
+    }
+    qlog.info('Found user: ' + JSON.stringify(user) + ', crt: ' + user.createdAt, filename);
+
+    var groupInfo = {};
+    groupInfo['groupOwner'] = Meteor.userId();
+    groupInfo['groupName'] = groupName;
+
+    if(user._id) {
+        Meteor.users.update({ "emails.address" : userEmail }, {$push: {groups : groupInfo}}, function(error){
+            if(error){
+                qlog.error('Error happened while pushing groups for user: ' + user._id + ', group: ' + groupName + ', ERROR: ' + error, filename);
+            } else {
+                qlog.info('Done pushing group: ' + groupName + ' for email: ' + userEmail, filename);
+            }
+        });
+    } else {
+        qlog.error('No user exists for email-id: ' + userEmail, filename);
+    }
+}
+
+
+updateUserGroupsCreated = function(groupName) {
+    var currUser = Meteor.users.findOne({_id : Meteor.userId()});
+    if(!currUser.groupsCreated) {
+        currUser.groupsCreated = [];
+    }
+
+    Meteor.users.update({ _id : Meteor.userId() }, {$push: {groupsCreated : groupName}}, function(error){
+        if(error){
+            qlog.error('Error happened while pushing owner groups for user: ' + currUser._id + ', group: ' + groupName + ', ERROR: ' + error, filename);
+        } else {
+            qlog.info('Done pushing owner group: ' + groupName , filename);
+        }
+    });
+}
