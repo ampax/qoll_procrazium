31ba005cfcb43cc01f8635b3924a4f5d00c7b271
cozenlabs <cozenlabs@gmail.com>
2014-03-14 19:46:01 -0400

    some cleanup

--- client/views/header/header_awesome.js
+++ client/views/header/header_awesome.js
@@ -1,7 +1,6 @@
 var filename = "client/views/header/header_awesome.js";
 Template.header_awesome.rendered = function()
-{
- // Close bootstrap's dropdown menu after clicking
+{// Close bootstrap's dropdown menu after clicking
     $('.collapse-onclick').each(function() {
         $(this).on("click", function () {
             var $obj = $($(this).parents('.in')[0]);
