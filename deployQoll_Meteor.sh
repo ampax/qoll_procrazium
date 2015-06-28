ee3dd0b599f8ced91d5296aae89e0d30953796d3
Anoop Kaushik <kaushik.anoop@gmail.com>
2015-06-28 15:52:14 -0400

    revised version of deploy to meteor script

--- deployQoll_Meteor.sh
+++ deployQoll_Meteor.sh
@@ -1,23 +1,59 @@
 #!/bin/sh
 
-echo 'Deploying at Meteor'
+echo 'Deploying at Meteor, options: '
 
-echo 'Enter environment identifier (dev or stage or prod):'
 
-read env_ident
+echo 'Check log or deploy app? '
+echo "		* log"
+echo "		* deploy"
 
-echo $env_ident
+read read_mode
 
-if [ "$env_ident" == 'dev' ] ; then
-	echo 'Deploying to qoll'$env_ident.meteor.com
-	sudo meteor deploy 'qoll'$env_ident.meteor.com
-elif [ "$env_ident" == 'stage' ] ; then
-	echo 'Deploying to qoll'$env_ident.meteor.com
-	sudo meteor deploy 'qoll'$env_ident.meteor.com
-elif [ "$env_ident" == 'prod' ] ; then
-	echo 'Deploying to qoll'$env_ident.meteor.com
-	sudo meteor deploy 'qoll'$env_ident.meteor.com
-else echo 'Enter dev or stage or prod. '$env_ident' is not recongnized.'
+echo $read_mode
+
+if [ "$read_mode" == "log" ] ; then
+	echo 'Enter environment identifier (dev or stage or prod) to read logs:'
+	echo "		** dev"
+	echo "		** stage"
+	echo "		** prod"
+
+	read env_ident
+
+	echo $env_ident
+
+	if [ "$env_ident" == "dev" ] ; then
+		echo 'Reading logs for qoll'$env_ident.meteor.com
+		sudo meteor logs 'qoll'$env_ident.meteor.com
+	elif [ "$env_ident" == "stage" ] ; then
+		echo 'Reading logs for qoll'$env_ident.meteor.com
+		sudo meteor logs 'qoll'$env_ident.meteor.com
+	elif [ "$env_ident" == "prod" ] ; then
+		echo 'Reading logs for qoll'$env_ident.meteor.com
+		sudo meteor logs 'qoll'$env_ident.meteor.com
+	else echo 'Enter dev or stage or prod. '$env_ident' is not recongnized for reading logs.'
+	fi
+elif [ "$read_mode" == "deploy" ] ; then
+	echo 'Enter environment identifier (dev or stage or prod) to deploy app:'
+	echo "		** dev"
+	echo "		** stage"
+	echo "		** prod'"
+
+	read env_ident
+
+	echo $env_ident
+
+	if [ "$env_ident" == "dev" ] ; then
+		echo 'Deploying to qoll'$env_ident.meteor.com
+		sudo meteor deploy 'qoll'$env_ident.meteor.com
+	elif [ "$env_ident" == "stage" ] ; then
+		echo 'Deploying to qoll'$env_ident.meteor.com
+		sudo meteor deploy 'qoll'$env_ident.meteor.com
+	elif [ "$env_ident" == "prod" ] ; then
+		echo 'Deploying to qoll'$env_ident.meteor.com
+		sudo meteor deploy 'qoll'$env_ident.meteor.com
+	else echo 'Enter dev or stage or prod. '$env_ident' is not recongnized for deplying app.'
+	fi
+else echo 'Enter log or deploy for mode. '$read_mode' is not recongnized.'
 fi
 
 # echo 'qoll'$1.meteor.com
